bf6aa31397f1a4725ccf4faad25b4fef70f6efc1
mayurs061089 <78772178+mayurs061089@users.noreply.github.com>
2023-05-18 19:49:17 -0400

    Update streamlit_app.py
    Added Header & Text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents new Healthy Diner')
+streamlit.header('BreakFast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale , Spinach & Rocket Smoothie')
+streamlit.title('Hard-Bolied Free-Range Egg')
+
